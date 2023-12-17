bc45adab2b5ebced6c20eb1e037089dbf10bcbb6
TobyM27 <tobyanto.121140099@student.itera.ac.id>
2023-12-17 18:06:09 +0700

    commit update on nakama.db

--- database/nakamaDB.sql
+++ database/nakamaDB.sql
@@ -1,3 +1,4 @@
+-- Berikut merupakan perintah untuk membuat struktur tabel 'nakama' 
 CREATE TABLE `nakama`(
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `karakter_favorit` VARCHAR(50) NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE `nakama`(
     `nilai_bounty` BIGINT(255) NOT NULL
 );
 
+-- memasuki beberapa data untuk tabel 'nakama'
 INSERT INTO `nakama` (`id`, `karakter_favorit`, `usia_karakter`, `devilfruit`, `asal_krew`, `nilai_bounty`) VALUES 
     (1,'Monkey D Luffy', 20, 'Gomu Gomu No Mi', 'Straw Hat Crew', 3000000000),
     (2,'Monkey D Garp', 40, 'fruitless', 'Marine', 0);
