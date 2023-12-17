118c40dbaa937b565e359c99211396f09cb7d0db
TobyM27 <tobyanto.121140099@student.itera.ac.id>
2023-12-17 18:04:46 +0700

    commit new update on nakama.db

--- database/nakamaDB.sql
+++ database/nakamaDB.sql
@@ -10,9 +10,3 @@ CREATE TABLE `nakama`(
 INSERT INTO `nakama` (`id`, `karakter_favorit`, `usia_karakter`, `devilfruit`, `asal_krew`, `nilai_bounty`) VALUES 
     (1,'Monkey D Luffy', 20, 'Gomu Gomu No Mi', 'Straw Hat Crew', 3000000000),
     (2,'Monkey D Garp', 40, 'fruitless', 'Marine', 0);
-
-
-
--- INSERT INTO `barang` (`seri`, `nama`, `merk`, `kategori`, `harga`, `stok`) VALUES
--- ('AS-1234', 'ASUS VGA RTX 2060 ', 'ASUS', 'Komponen', 3500000, 32),
--- ('HP-1322', 'HP OMEN 2023', 'HP', 'Laptop', 12000000, 20);
