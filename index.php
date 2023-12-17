a8cfd66dd28a437b3318a1864944b9a78836f7a3
TobyM27 <tobyanto.121140099@student.itera.ac.id>
2023-12-17 16:22:21 +0700

    commit update on index page

--- index.php
+++ index.php
@@ -11,9 +11,27 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Website Nakama One Piece </title>
 </head>
 <body>
-    <p> testing </p>
+    <p class="info-login"> Berikut merupakan credential bawaan <br> Username : Nakama <br> Password : May5 </p> 
+    
+    <form action="login.php" method="post">
+        <!-- Buat image nakama disini dengan photo luffy <img src> -->
+        <h2> Login untuk jadi Nakama Luffy </h2>
+        <?php if (isset($_SESSION['Error'])) { ?>
+            <p class="Error">
+                <?php echo $_SESSION['Error']; ?>
+            </p> 
+            <?php unset($_SESSION['Error']); ?>
+        <?php } ?>
+        <label> Username : </label>
+        <input type="text" name="username" placeholder="Inputkan username">
+
+        <label> Password : </label>
+        <input type="password" name="password" placeholder="Inputkan password">
+
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
